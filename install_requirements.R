62ee7441d76ca87c4c045816a1f7675d27de18a4
danielturek <danielturek@gmail.com>
2021-11-18 17:47:49 -0500

    updated install_requirements praparing for ADoak branch of nimble

--- install_requirements.R
+++ install_requirements.R
@@ -7,10 +7,11 @@ requirements <- c(
     'nimble',
     'testthat')
 
-for (package in requirements) {
-    if (!suppressPackageStartupMessages(require(package,
-                                                character.only = TRUE))) {
+##library(devtools)
+##devtools::install_github('nimble-dev/nimble', ref = 'devel', subdir = 'packages/nimble')
+
+for(package in requirements) {
+    if(!suppressPackageStartupMessages(require(package, character.only = TRUE)))
         install.packages(package, repos = 'http://cran.us.r-project.org')
-    }
 }
 
