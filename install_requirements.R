4f30017c1ae91d0e6731a1e0ea449bc37950e444
danielturek <danielturek@gmail.com>
2021-11-21 07:56:57 -0500

    changed package installations to type = source

--- install_requirements.R
+++ install_requirements.R
@@ -9,11 +9,12 @@ requirements <- c(
 
 for(package in requirements) {
     if(!suppressPackageStartupMessages(require(package, character.only = TRUE)))
-        install.packages(package, repos = 'http://cran.us.r-project.org')
+        ##install.packages(package, repos = 'http://cran.us.r-project.org')
+        install.packages(package, type = 'source')
 }
 
 ## XXXXXXXXXXXXXXXXXXX remove below
-install.packages('devtools', repos = 'http://cran.us.r-project.org')
+install.packages('devtools', type = 'source')
 library(devtools)
 devtools::install_github('nimble-dev/nimble', ref = 'ADoak_without_HMC', subdir = 'packages/nimble')
 ## XXXXXXXXXXXXXXXXXX remove until here
