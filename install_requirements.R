c23bb04e6106e72731775f8399bcb3acc31f7c52
danielturek <danielturek@gmail.com>
2022-06-15 12:01:22 -0400

    loading all packages

--- install_requirements.R
+++ install_requirements.R
@@ -4,29 +4,34 @@ library_install_path <- .libPaths()[1L]
 
 ## XXXXXX remove this:
 print('RUNNING INSTALL_REQUIREMENTS.R')
-print('library_install_path:')
-print(library_install_path)
-install.packages('igraph')
-a <- library(igraph, lib.loc = library_install_path)
-print('installed igraph:')
-print(a)
+print(paste0('library_install_path:', library_install_path))
+print('BBBB')
+##
+###install.packages('igraph')
+###a <- library(igraph, lib.loc = library_install_path)
+###print('installed igraph:')
+###print(a)
+#####
+###install.packages('coda')
+###a <- library(coda, lib.loc = library_install_path)
+###print('installed coda:')
+###print(a)
 
 
 
-##### XXXXXXXXX uncomment below:
-##requirements <- c(
-##    'igraph',
-##    'coda',
-##    'R6',
-####    'nimble',    ## XXXXXX add this line back in
-##    'testthat')
+requirements <- c(
+    'igraph',
+    'coda',
+    'R6',
+##    'nimble',    ## XXXXXX add this line back in
+    'testthat')
 
-####### XXXXXXXXX uncomment below:
-##for(package in requirements) {
-##    if(!suppressPackageStartupMessages(require(package, character.only = TRUE)))
-##        ##install.packages(package, repos = 'http://cran.us.r-project.org')
-##        install.packages(package, type = 'source')
-##}
+for(package in requirements) {
+    if(!suppressPackageStartupMessages(require(package, character.only = TRUE)))
+        ##install.packages(package, repos = 'http://cran.us.r-project.org')
+        ##install.packages(package, type = 'source')
+        install.packages(package)
+}
 
 ####### XXXXXXXXX uncomment below:
 #### XXXXXXXXXXXXXXXXX remove below
@@ -34,4 +39,16 @@ print(a)
 ##library(devtools)
 ##devtools::install_github('nimble-dev/nimble', ref = 'ADoak', subdir = 'packages/nimble')
 #### XXXXXXXXXXXXXXXX remove until here
+
+
+
+
+## XXXXXX remove this:
+a <- library(igraph, lib.loc = library_install_path)
+a <- library(coda, lib.loc = library_install_path)
+a <- library(R6, lib.loc = library_install_path)
+a <- library(testthat, lib.loc = library_install_path)
+print('loaded packages:')
+print(a)
+
 
