3176d1a6583137a66eb91dd582b23abc6ae2faf0
danielturek <danielturek@gmail.com>
2022-08-25 12:29:50 -0400

    updated mcmc_createModelObject with buildDerivs option

--- make_package.R
+++ make_package.R
@@ -3,7 +3,7 @@ library(devtools)
 
 ## NECESSARY:
 ## NEED TO HAVE DONE "build" bash command while on correct
-## branch of nimble repository (currently: ADoak))
+## branch of nimble repository (currently: AD-rc0)))
 
 ## create github actions testing
 ##library(usethis)
